9ae0508ae5536cf559c47e1d58a2860714424585
Frank Zhao <frank@frankzhao.net>
2021-04-22 17:57:12 +1000

    Remove unecessary permissions
    Limit to only one timer.

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
+let refreshTimerSet = false;
 chrome.runtime.onMessage.addListener(function (request, sender, response) {
     const bambooDomain = sender.origin;
-    console.log("Keeping alive BambooHR session...")
-    setInterval(function () {
-        fetch(bambooDomain).then(r => r.text()).then(_ => {
-            console.log('Refreshed BambooHR.');
-        });
-    }, 1000 * 60 * 25)
+    if (!refreshTimerSet) {
+        setInterval(function () {
+            console.log("Keeping alive BambooHR session...");
+            fetch(bambooDomain).then(r => r.text()).then(_ => {
+                console.log('Refreshed BambooHR.');
+            });
+        }, 1000 * 60 * 25)
+        refreshTimerSet = true;
+    }
 });
